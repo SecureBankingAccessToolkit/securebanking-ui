fa606fdf44bcbb481799b4e8502988f95b530741
Dan Badham <dan.badham@forgerock.com>
2023-02-09 09:39:14 +0000

    793: Configure angular --base-href to match ingress rule of /rcs/ui/
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/805

--- securebanking-cli-ui/scripts/buildThemes.js
+++ securebanking-cli-ui/scripts/buildThemes.js
@@ -72,6 +72,8 @@ async function build(project, customer) {
       "build",
       "--project",
       project,
+      "--base-href",
+      "/rcs/ui/",
       "--configuration",
       customer,
       "--output-path",
