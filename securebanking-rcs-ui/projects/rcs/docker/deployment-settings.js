1797dc7359f4898b82e626dc95a511c9eca93d23
Dan Badham <dan.badham@forgerock.com>
2023-02-09 14:51:04 +0000

    805: Fixing start.sh to replace IG_FQDN placeholder in deployment-settings.js with env var value
    Also removing igServer deployment setting, as it is not used.
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/805

--- securebanking-rcs-ui/projects/rcs/docker/deployment-settings.js
+++ securebanking-rcs-ui/projects/rcs/docker/deployment-settings.js
@@ -4,6 +4,5 @@ module.exports = {
     template: 'TEMPLATE'
   },
   authorizationServer: 'https://IDENTITY_PLATFORM_FQDN',
-  remoteConsentServer: 'https://IG_FQDN',
-  igServer: 'https://IG_FQDN',
+  remoteConsentServer: 'https://IG_FQDN'
 };
