ca49e6bdb067be35ef6ac152cd95fd9a1959c1c0
Dan Badham <dan.badham@forgerock.com>
2023-02-09 14:34:17 +0000

    805: Configure UI to make calls to RCS API via IG route
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/805

--- securebanking-rcs-ui/projects/rcs/docker/deployment-settings.js
+++ securebanking-rcs-ui/projects/rcs/docker/deployment-settings.js
@@ -4,6 +4,6 @@ module.exports = {
     template: 'TEMPLATE'
   },
   authorizationServer: 'https://IDENTITY_PLATFORM_FQDN',
-  remoteConsentServer: 'https://RCS_FQDN',
+  remoteConsentServer: 'https://IG_FQDN',
   igServer: 'https://IG_FQDN',
 };
