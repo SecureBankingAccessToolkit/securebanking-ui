28bc0ad3e1bbe20dbb00d718ea58ee9839958791
jorgesanchezperez <jorge.sanchez-perez@forgerock.com>
2023-07-20 13:03:51 +0100

    Updated customer info mock

--- secure-api-gateway-ob-uk-ui-rcs/projects/rcs/src/app/pages/consent-dev/mocks/customer-info-details.js
+++ secure-api-gateway-ob-uk-ui-rcs/projects/rcs/src/app/pages/consent-dev/mocks/customer-info-details.js
@@ -1,28 +1,37 @@
 module.exports = {
-  intentType: "CUSTOMER_INFO_CONSENT",
   type: "CustomerInfoConsentDetails",
+  "consentId": "CIC_a337e1a0-53ab-478b-b59a-1d68d5f07244",
   decisionApiUri: "/rcs/api/consent/decision/",
-  username: "psu4test",
-  userId: "fc99f546-8507-428b-88f2-1d78860a0881",
-  logo: "https://forgerock.com",
-  clientId: "225ca346-85b0-43cf-91e5-e59c67619c65",
-  clientName: "TPP Test application",
-  serviceProviderName: "Forgerock Bank simulation config",
+  username: "bab001",
+  userId: "61d9f601-4003-44b6-ab47-5b5eb83f85fb",
+  clientId: "5f996ff6-1b44-47a3-9651-0cc5946aeca7",
+  clientName: "Automating-testing",
+  serviceProviderName: "Test Bank",
+  permissions: [
+    "ReadCustomerInfoPSU"
+  ],
   customerInfo: {
-    familyName: "Doe",
-    givenName: "Joe",
-    email: "Joe.doe@acme.com",
-    phoneNumber: "07743 233223",
-    birthdate: "16/02/1987",
+    id: "64b908af2b21e25541392293",
+    userID: "61d9f601-4003-44b6-ab47-5b5eb83f85fb",
+    partyId: "32432-3242",
+    title: "Mr",
+    initials: "F",
+    familyName: "Titmus",
+    givenName: "Fred",
+    email: "fred.titmus@acme.com",
+    phoneNumber: "07743 234323",
+    birthdate: "16/02/2001",
     address: {
+      addressType: "Residential",
       streetAddress: [
         "999",
-        "River Side",
-        "Bradford",
-        "Wiltshire"
+        "letsbe avenue",
+        "colchester",
+        "essex"
       ],
-      postalCode: "BA10 5BW",
+      postalCode: "CO8 3JJ",
       country: "UK"
     }
-  }
-};
+  },
+  intentType: "CUSTOMER_INFO_CONSENT"
+}
